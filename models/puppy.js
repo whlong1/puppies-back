c651f798353262bf82221dc749c2fa59340d9af7
whlong1 <whunterlong@gmail.com>
2022-11-07 10:38:28 -0500

    Build out puppySchema

---
+++ models/puppy.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const puppySchema = new Schema({
+  name: { type: String, required: true },
+  age: { type: Number, default: 0 },
+  breed: { type: String, default: "Mixed" },
+  ears: {
+    type: String,
+    default: 'Button',
+    enum: ['Button', 'Drop', 'Bat']
+  },
+  color: {
+    type: String,
+    default: 'Grey',
+    enum: ['Grey', 'Black', 'Brown', 'White', 'Golden']
+  },
+}, {
+  timestamps: true
+})
+
+const Puppy = mongoose.model('Puppy', puppySchema)
+
+export {
+  Puppy
+}
