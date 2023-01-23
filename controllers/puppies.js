fe9f341550ef195bb9331f1aab37671e48b3c6fc
whlong1 <whunterlong@gmail.com>
2023-01-23 10:24:26 -0500

    Update index controller

--- controllers/puppies.js
+++ controllers/puppies.js
@@ -1,24 +1,30 @@
 import { Puppy } from '../models/puppy.js'
 
 const index = async (req, res) => {
-  const page = req.query.page
-  console.log(page)
-
-  const puppy = await Puppy.find({})
-    .limit(8)
-    .skip(parseInt(page) * 8)
-    .sort({ createdAt: 'desc' })
-  res.status(200).json(puppy)
+  try {
+    const puppy = await Puppy.find({}).sort({ createdAt: 'desc' })
+    res.json(puppy)
+  } catch (err) {
+    res.json(err)
+  }
 }
 
 const create = async (req, res) => {
-  const puppy = await Puppy.create(req.body)
-  res.status(200).json(puppy)
+  try {
+    const puppy = await Puppy.create(req.body)
+    res.status(200).json(puppy)
+  } catch (err) {
+    res.json(err)
+  }
 }
 
 const deletePuppy = async (req, res) => {
-  const puppy = await Puppy.findByIdAndDelete(req.params.id)
-  res.status(200).json(puppy)
+  try {
+    const puppy = await Puppy.findByIdAndDelete(req.params.id)
+    res.status(200).json(puppy)
+  } catch (err) {
+    res.json(err)
+  }
 }
 
 export {
